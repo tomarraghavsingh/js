ba63c43e4a6836604b2ad7806debf6271c59fb38
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-02-23 19:01:15 +0530

    Add files via upload

---
+++ 1st.js
@@ -0,0 +1,13 @@
+document.write("myself Raghav Singh Tomar")
+console.log("dfgfhh")
+alert("hello")
+
+let a=10
+let b=20
+let c
+console.log(c=a+b)
+ let num=parseInt(prompt("enter 1st no : "))
+ let num1=parseInt(prompt("enter 2nd no :"))
+ console.log(num1+num)
+ let sq=(num+num1)*(num+num1)
+ console.log(sq)
