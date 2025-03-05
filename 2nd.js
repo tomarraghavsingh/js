e6cc5d24acabd29404c3fc4ce959f48c99f2658f
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-03-06 02:08:34 +0530

    Add files via upload

---
+++ 2nd.js
@@ -0,0 +1,26 @@
+// WAP to find the area of triangle
+// let Areaofcircle=(r)=>{
+//     let pi=3.14
+//     return pi*r*r
+// }
+// console.log(Areaofcircle(10))
+
+
+let r=parseInt(pro)
+
+// //wap to swap two value using third variable//
+// let a = 15;
+// let b = 20;
+
+// let num = a;
+// a = b;
+// b = num;
+// console.log(a,b);
+
+
+//wap to add two number and find squre of answer//
+let num = parseInt(prompt("Enter First Number"))
+let num1 = parseInt(prompt("Enter Second  Number"))
+let num2 = num+num1;
+console.log(num+num1);
+console.log(num2*2)
