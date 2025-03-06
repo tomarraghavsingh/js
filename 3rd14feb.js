72b761e9b3e028fabc6fd94eed8cce788b48a4e7
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-03-06 15:14:46 +0530

    Add files via upload

---
+++ 3rd14feb.js
@@ -0,0 +1,27 @@
+// let num=parseInt(prompt("enter the num"))
+// if(num>0){
+//     console.log("positive")
+// }
+// else{
+//     console.log("negative")
+// }
+// wap to check even or odd
+// let num1=parseInt(prompt("enter the num"))
+// if(num1%2==0){
+//   console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+// wap to check num is odd or even if num is odd then find are of circle and if num is even then find cube of that num
+let num2=parseInt(prompt("enter the number : "))
+let area
+let cube
+if(num2%2!=0){
+console.log("odd")
+console.log(area=3.14*num2*num2)
+}
+else{
+    console.log("even")
+    console.log(cube=num2*num2*num2)
+}
