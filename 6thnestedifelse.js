e00687fb681f14b0134c45043c676c4097529a40
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-03-24 00:07:57 +0530

    Add files via upload

---
+++ 6thnestedifelse.js
@@ -0,0 +1,17 @@
+let age=parseInt(prompt("enter your age: "))
+let num
+if(age>=18){
+    let num=parseInt(prompt("press 1 for indian"))
+    
+    if(num=1){
+        console.log("eligible")
+    }
+    else{
+        console.log("not elegible")
+    }
+
+
+}
+else{
+    console.log("u r child")
+}
