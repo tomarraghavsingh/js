a479b8a570a54c40f6c27524516bafe37e8ba470
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-03-25 11:45:59 +0530

    Add files via upload

---
+++ 7th17febloop.js
@@ -0,0 +1,22 @@
+// do while loop
+// do{
+
+// }
+// while(condition)
+// var a=1
+// do{
+//     document.write("raghav <br>")
+//     a++
+// }
+// while(a<11)
+// var num=1
+// do{
+//     document.write(num)
+//     num++
+// }
+// while(num<11)
+let a
+do{
+    document.write(a)
+}
+while(a>11)
