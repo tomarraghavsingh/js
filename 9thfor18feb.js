483bef4811a8b7c9a64a109c4e5d46cd4a83a653
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-03-27 09:48:42 +0530

    Add files via upload

---
+++ 9thfor18feb.js
@@ -0,0 +1,12 @@
+// for(let a=1;a<=5;a++){
+//     console.log(a);
+//     document.write(a);
+// }
+// WAP TO PRINT ODD NUM BTW 1 TO20
+// let odd=;
+// for(let a=1;a<=20;a++){
+//     console.log(odd=a%2)
+// }
+for(let b=20;b>=1;b--){
+    document.write("<h1>"+b+"</h1>")
+}
