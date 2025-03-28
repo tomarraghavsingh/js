64cc511f0082ee66c7848f5f2c42d8c3fcdc4e47
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-03-28 10:27:58 +0530

    Add files via upload

---
+++ 10thfunction.js
@@ -0,0 +1,20 @@
+function Sum(){
+    let a=10;
+    let b=20;
+    document.write(a+b)
+}
+Sum()
+function Sub(){
+    let a=10;
+    let b=20;
+    document.write(a-b)
+}
+Sub()
+<br>
+function Table(){
+    let n=parseInt(prompt("enter table digit"))
+    for(i=1;i<11;i++){
+        document.write(n*i)
+    }
+}
+Table()
