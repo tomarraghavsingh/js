5f561121dc6fb32184f302a3d4aa9d804c17cc54
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-04-01 21:51:35 +0530

    Add files via upload

---
+++ 11th19febfunction.js
@@ -0,0 +1,13 @@
+// WAP to check num is even or odd using type 2 function
+// with argu and no return
+function Evenorodd(){
+
+if(num%2==0){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+}
+let num=parseInt(prompt("enter any number"))
+Evenorodd()
