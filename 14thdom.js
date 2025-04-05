474cc0b254923b60b1a89f1882062e06effbec0d
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-04-05 23:44:41 +0530

    Add files via upload

---
+++ 14thdom.js
@@ -0,0 +1,13 @@
+let a=document.getElementById('head')
+console.log(a);
+let b=document.getElementsByClassName('pro')
+console.log(b)
+let c=document.getElementsByTagName('h2')
+console.log(c)
+let d=document.getElementById('tail')
+console.log(d)
+d.textContent="its me raghav"
+a.innerHTML="<i>changed by raghav</i>"
+let e=document.getElementsByTagName('h4')
+console.log(e)
+e[1].innerHTML="rst"
