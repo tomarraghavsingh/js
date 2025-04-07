7ba8e15dad087ed6bd3b35202559b81949f56aa3
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-04-07 19:17:40 +0530

    Add files via upload

---
+++ 15thdom.js
@@ -0,0 +1,10 @@
+let image=document.getElementById('id')
+image.src = "tree-736885_1280.jpg"
+let head=document.getElementById('head')
+head.style.color="red"
+let a=document.getElementById('para')
+a.innerHTML="<b>hello</b>"
+
+let btn=document.getElementById('submit')
+btn.style.backgroundColor="lightblue"
+btn.style.padding="20px"
