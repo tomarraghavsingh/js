d0f9a2c07baa45c9f2f93ba38547f550d8c457dc
Raghav Singh Tomar <162915611+tomarraghavsingh@users.noreply.github.com>
2025-04-09 20:36:09 +0530

    Add files via upload

---
+++ 17th.js
@@ -0,0 +1,15 @@
+function fun(){
+    alert("working")
+}
+function fun1(){
+    let head=document.getElementById('id1')
+    head.textContent="zero"
+}
+function fun(){
+    let color=document.getElementById('red')
+    color.setAttribute('bgcolor''red')
+}
+// function fun1(){
+//     let head=document.getElementById('id1')
+//     head.textContent="hero"
+// }
